b32e2c2366eca23093cef6f1a611b35bd389e7f9
mostafa <mostafayassr21@gmail.com>
2023-06-16 14:59:46 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - enty point
+ *
+ * description: A c programing
+ *
+ * return: always 0(success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
