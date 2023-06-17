65e628a3818f2fe0036ef0f56807b10613ce6c38
mostafa <mostafayassr21@gmail.com>
2023-06-17 01:05:31 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,12 @@
  *
  * Description : creating a text using wright function
  *
- * Return : 1 (not success)
+ * Return : 1 (Error)
 */
 
 int main(void)
-{
-	char quo[] ="and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
-	write(1,quo,59);
+{   char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
 
 	return (1);
-
 }
