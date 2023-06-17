48c7a5210799c26ece75059fe5047194162d453b
mostafa <mostafayassr21@gmail.com>
2023-06-16 23:59:41 -0700

    a

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Main : entry level
+ *
+ *Description : a C prgram that prits with printf function
+ *
+ * Return : always 0 (success)
+*/
+
+int main(void)
+{
+	printf(with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
