31ad6b06941882eaf2cbd7fa07f86d905f69f5a1
mostafa <mostafayassr21@gmail.com>
2023-06-17 00:20:04 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 /**
- * Main : entry level
+ *Main : entry level
  *
  *Description : a C prgram that prits with printf function
  *
- * Return : always 0 (success)
+ *Return : always 0 (success)
 */
 
 int main(void)
 {
-	printf(with proper grammar, but the outcome is a piece of art,\n");
+	printf("size of a chart:%lu byte(s)\n", sizeof(char));
+	printf("size of an:%lu byte(s)\n", sizeof(int));
+	printf("size of a long int:%lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int:%lu byte(s)\n", sizeof(long long int));
+	printf("size of a float:%lu byte(s)\n", sizeof(float));
 	return (0);
+
 }
