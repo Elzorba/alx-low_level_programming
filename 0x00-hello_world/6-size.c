ca1a11b6d60626c7eeb93291f05e935cc2994215
mostafa <mostafayassr21@gmail.com>
2023-06-17 00:25:25 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  *Main : entry level
  *
- *Description : a C prgram that prits with printf function
+ *Description : useing sizeof to print the size of various size
  *
  *Return : always 0 (success)
 */
@@ -11,10 +11,10 @@
 int main(void)
 {
 	printf("size of a chart:%lu byte(s)\n", sizeof(char));
-	printf("size of an:%lu byte(s)\n", sizeof(int));
-	printf("size of a long int:%lu byte(s)\n", sizeof(long int));
-	printf("size of a long long int:%lu byte(s)\n", sizeof(long long int));
-	printf("size of a float:%lu byte(s)\n", sizeof(float));
+	printf("size of an: %lu byte(s)\n", sizeof(int));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 
 }
