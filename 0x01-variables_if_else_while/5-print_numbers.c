4507347bff01ba97fe05b5a97568c6ce27b55afc
mostafa <mostafayassr21@gmail.com>
2023-06-20 05:46:07 -0700

    a

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,16 +8,18 @@
  * return - always 0 (success)
 */
 
-int main(void)
-{
+int main(void) 
+	
+  {
 	int digit = 0;
 
 	while (digit <= 9)
 	{
 		printf("%i", digit);
+
 		digit++;
 	}
 	printf("\n")
 
         return (0);
-}
+   }
