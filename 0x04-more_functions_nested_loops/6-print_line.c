9157a825f01405b74e9eabb1cd57daff61054978
mostafa <mostafayassr21@gmail.com>
2023-06-23 14:05:35 -0700

    a

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * ptint_line print a stright line 
+ *
+ * @n:is how meny time the character should prints 
+ */
+void print_line(int n)
+{
+int lnChr;
+
+if (n <= 0)
+_putchar('\n');
+else 
+{
+for (lnChr = 1; lnChar <= n; lnChr++)
+_putchar('_');
+_putchar('\n');
+}
+}
