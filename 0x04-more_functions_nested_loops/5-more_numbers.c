b3d4b883e552d24193281113244b61e27bee655a
mostafa <mostafayassr21@gmail.com>
2023-06-23 13:36:19 -0700

    a

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers print 0 to 14 te times
+ *
+ *return 0 mean success
+ */
+void more_numbers(void)
+{
+	int num, row, count;
+
+	for (row = 1; row <= 10; row++)
+	{
+		for (count = 0; cont <= 14; count++)
+		{
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
+		}
+		_putchar('\n');
+	}
+}
