8656b630896d3bc96853476be18606d76e39ca3d
mostafa <mostafayassr21@gmail.com>
2023-06-27 14:00:24 -0700

    a

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen: return the length of string
+ *
+ * @s:the sting paramater input
+ *
+ * return: the lenght of string
+ */
+int _strlen(char *s)
+{
+	int counter;
+        
+	for(counter = 0; *s != '\0'; s++)
+		++counter;
+	return (counter);
+}
