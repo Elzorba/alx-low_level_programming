51614f2f897b277bed8c76398b1ea26379336c98
mostafa <mostafayassr21@gmail.com>
2023-06-27 12:38:57 -0700

    a

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,11 @@
 #include "main.h"
-/***/
+/**
+ * reset_to_98: takes a pointer to int equal 98
+ *
+ * @n : is the character name to the  pointer
+ *
+ * return: nothing
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
