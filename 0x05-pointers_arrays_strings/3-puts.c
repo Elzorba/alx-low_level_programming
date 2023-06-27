7f95a77e681451a6f1a6b3b0aa32c1e5010ddf71
mostafa <mostafayassr21@gmail.com>
2023-06-27 14:27:15 -0700

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - print a string followed by new line at stdout
+ *
+ * str: string is a parametar input
+ *
+ * return: nothing
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		 _putchar(*str + 0);
+		 str++;
+
+	}
+	_putchar ("\n");
+}
