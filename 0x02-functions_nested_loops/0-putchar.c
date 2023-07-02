a5162a56fe9d39d192cc145e46b7f84c08c7eccd
mostafa <mostafayassr21@gmail.com>
2023-07-02 11:39:40 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main entry point
  *
@@ -9,11 +10,11 @@
 
 int main(void)
 {
-	char str[] ="_putchar";
+	char str[] = "_putchar";
 	int ch;
 
 	for (ch = 0; ch < 8; ch++)
-		_putchar(str[ch]);
-		_putchar('\n');
+		putchar(str[ch]);
+		putchar('\n');
 	return (0);
 }
