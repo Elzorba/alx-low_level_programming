c02f6256cc49c00b0aab57960db12e0662f32a04
mostafa <mostafayassr21@gmail.com>
2023-07-02 12:19:35 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main : intry point  print alphabet using put char
+ * return : if 0 succes
+ */
+void print_alphabet(void)
+{
+        int ch;
+
+        for (ch = 'a'; ch <= 'z'; ch++)
+         _putchar(ch);
+         _putchar("\n");
+}
+        
